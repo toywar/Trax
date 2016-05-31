2e291779bdf796398f5fefb96ed11af96c80d0cc
Роман Екимов <roman.ekimov@icloud.com>
2016-05-31 23:54:55 +0300

    Initial Commit

---
+++ Trax/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trax
+//
+//  Created by Роман Екимов on 31.05.16.
+//  Copyright © 2016 Роман Екимов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
