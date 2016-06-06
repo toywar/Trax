5ec77e0e8a73a25b4dea7c6c33096e9b529a95a5
romek <romek@toywar.ru>
2016-06-07 01:02:57 +0300

    complete lesson 15

---
+++ Trax/EditWaypointViewController.swift
@@ -0,0 +1,73 @@
+//
+//  EditWaypointViewController.swift
+//  Trax
+//
+//  Created by Roman Ekimov on 06.06.16.
+//  Copyright © 2016 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class EditWaypointViewController: UIViewController, UITextFieldDelegate {
+    
+    var waypointToEdit: EditableWaypoint? { didSet { updateUI() } }
+
+    @IBOutlet weak var nameTextField: UITextField! { didSet { nameTextField.delegate = self } }
+    @IBOutlet weak var infoTextField: UITextField! { didSet { infoTextField.delegate = self } }
+    
+    func updateUI() {
+        nameTextField?.text = waypointToEdit?.name
+        infoTextField?.text = waypointToEdit?.info
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameTextField.becomeFirstResponder()
+        updateUI()
+    }
+    
+    
+    @IBAction func done(sender: UIBarButtonItem) {
+        presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        observeTextFields()
+    }
+    
+    var ntfObserver: NSObjectProtocol?
+    var itfObserver: NSObjectProtocol?
+    
+    func observeTextFields() {
+        let center = NSNotificationCenter.defaultCenter()
+        let queue = NSOperationQueue.mainQueue()
+        ntfObserver = center.addObserverForName(UITextFieldTextDidChangeNotification, object: nameTextField, queue: queue) { notification in
+            if let waypoint = self.waypointToEdit {
+                waypoint.name = self.nameTextField.text
+            }
+        }
+        itfObserver = center.addObserverForName(UITextFieldTextDidChangeNotification, object: infoTextField, queue: queue) { notification in
+            if let waypoint = self.waypointToEdit {
+                waypoint.info = self.infoTextField.text
+            }
+        }
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        if let observer = ntfObserver {
+            NSNotificationCenter.defaultCenter().removeObserver(observer)
+        }
+        if let observer = itfObserver {
+            NSNotificationCenter.defaultCenter().removeObserver(observer)
+        }
+    }
+    
+    func textFieldShouldReturn (textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+
+}
